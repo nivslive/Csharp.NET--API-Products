6ee913ee9758bb1edec97656977f5d3dbba9bb02
Nivs <nivsoficial@gmail.com>
2023-07-30 17:10:51 -0300

    atts about sqlserver

---
+++ API-Products/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using API_Products;
+namespace API_Products.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+    public DbSet<Product> Products { get; set; }
+}
+
